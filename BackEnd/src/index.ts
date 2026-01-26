84dbf4b65d6397db11fab098cef1b0132c6cd79a
Erika <erikalopespro@gmail.com>
2026-01-25 21:38:20 -0300

    Adiciona CORS no backend e integra frontend com backend via api.ts, organizando endpoints de favoritos

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import favoritesRoutes from './routes/favoritesRoutes';
+import cors from 'cors'
 
 const app = express();
 const PORT = 3000;
@@ -13,4 +14,20 @@ app.use('/favorites', favoritesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+
+// Middleware para permitir requisições do Frontend
+app.use(cors({
+  origin: 'http://localhost:5173' // porta do  frontend
+}));
+
+// Middleware para ler JSON no corpo das requisições
+app.use(express.json());
+
+// Rotas
+app.use('/favorites', favoritesRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
